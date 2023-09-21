2fc35a3feeaba44c4763ae3e54c1344e54379a28
josh <77705398+joshk777@users.noreply.github.com>
2023-09-21 19:45:51 -0400

    javadoc & input validation
    updates to the driver javadoc
    added input validation to mana & health

--- GameCharacter/src/gamecharacter/GameCharacter.java
+++ GameCharacter/src/gamecharacter/GameCharacter.java
@@ -1,69 +1,68 @@
 package gamecharacter;
 
 /**
-* The GameCharacter class represents a basic character with attributes such as name and health.
-* This class can be extended to create specialized character types.
-*@author Joshua Klenk
-*@version 1.0
-*/
-
+ * The GameCharacter class represents a basic character with attributes such as name and health.
+ * This class can be extended to create specialized character types.
+ *@author Joshua Klenk
+ *@version 1.0
+ */
 public class GameCharacter 
 {
-    
+
     private String characterName; //name of character.
     private int health; // The current health points of the character.
-    
+
     /**
-    *Constructs a basic character that receives a specific name and health points.
-    *
-    *Initializes the basic character information such as health & name.
+     *Constructs a basic character that receives a specific name and health points.
+     *
+     *Initializes the basic character information such as health & name.
      * @param newName name of the character being created.
      * @param initialHealth beginning health points.
      * 
-    */
-    public GameCharacter(String newName, int initialHealth)
+     */
+    public GameCharacter(String newName, int initialHealth) 
     {
         characterName = newName;
         health = initialHealth;
-        
+
     }
-    
+
     /**
-    *Sets the name of the character.
-    *
+     *Sets the name of the character.
+     *
     @param newName The name to be assigned to the character.
-    */
-    public void setName(String newName)
+     */
+    public void setName(String newName) 
     {
         characterName = newName;
     }
-    
+
     /**
-    * Sets the health points of the character .
-    *
+     * Sets the health points of the character .
+     *
     @param initialHealth The health points to be assigned to the character.
-    */
-    public void setHealth(int initialHealth)
+     */
+    public void setHealth(int initialHealth) 
     {
         health = initialHealth;
     }
-    
+
     /**
-    * Retrieves the name of the character.
-    *
+     * Retrieves the name of the character.
+     *
     @return The name of the character as a string.
-    */
-    public String getName()
+     */
+    public String getName() 
     {
-      return characterName;  
+        return characterName;
     }
-    
+
     /**
-    * Returns the current health points of the character.
-    *
+     * Returns the current health points of the character.
+     *
     @return The current health points as an integer.
-    */
-    public int getHealth()
+     */
+    public int getHealth() 
     {
         return health;
     }
--- GameCharacter/src/gamecharacter/GameCharacterDriver.java
+++ GameCharacter/src/gamecharacter/GameCharacterDriver.java
@@ -1,55 +1,76 @@
 package gamecharacter;
 
 import java.util.Scanner;
-/**
- * @author Joshua Klenk
- */
 
+/**
+ *The Game Character driver is a driver program for creating and interacting with
+ *various characters
+ *
+ * @author Joshua Klenk
+ *@version 1.0
+ */
 public class GameCharacterDriver 
 {
+
     public static void main(String[] args) 
     {
-        //instantiate the scanner object for user input
+        //Instantiate the scanner object for user input.
         Scanner keyboard = new Scanner(System.in);
-        
-        //initialize variables for user input
-        int updateMana,updateHealth;
+
+        //Initialize variables for user input.
+        int updateMana = -1, updateHealth = -1;
         String newName, newSpell;
-        
-        //creates a character object of the mage type
+
+        //Creates a character object of the mage type.
         Mage myCharacter = new Mage("Jackal", 200, 100, "Firebolt");
 
-        //prints the hard coded information for the mage
+        //Prints the hard coded information for the mage.
         System.out.printf("\n%-20s %-20s %-20s %-20s\n", "Character Name", "Total Health", "Total Mana", "Spell");
-        System.out.printf("%-20s %-,20d %-,20d %-20s\n", myCharacter.getName(), myCharacter.getHealth(), 
+        System.out.printf("%-20s %-,20d %-,20d %-20s\n", myCharacter.getName(), myCharacter.getHealth(),
                 myCharacter.getMana(), myCharacter.getSpell());
-        
-        //gather user input for updating the mage object
+
+        //Gather user input for updating the mage object.
         System.out.print("\nUpdate Character Name: ");
         newName = keyboard.nextLine();
         myCharacter.setName(newName);
+
+        while(updateHealth < 0)
+        {
+            System.out.print("\nUpdate new health value: ");
+            updateHealth = keyboard.nextInt();
+            myCharacter.setHealth(updateHealth);
+            keyboard.nextLine();
+            
+            if(updateHealth < 0)
+            {
+                System.out.println("Please enter a non negative number.");
+            }
+        }
         
-        System.out.print("\nUpdate new health value: ");
-        updateHealth = keyboard.nextInt();
-        myCharacter.setHealth(updateHealth);
-        keyboard.nextLine();
-        
-        System.out.print("\nUpdate new mana value: ");
-        updateMana = keyboard.nextInt();
-        myCharacter.setMana(updateMana);
-        keyboard.nextLine();
+        while(updateMana < 0)
+        {
+            System.out.print("\nUpdate new mana value: ");
+            updateMana = keyboard.nextInt();
+            myCharacter.setMana(updateMana);
+            keyboard.nextLine();
+            
+            if(updateMana < 0)
+            {
+                System.out.println("Please enter a non negative number.");
+            }
+        }
         
         System.out.print("\nUpdate chosen spell: ");
         newSpell = keyboard.nextLine();
         myCharacter.setSpell(newSpell);
         System.out.println();
 
-        //print the updated results
+        //Print the updated results.
         System.out.printf("\n%-20s %-20s %-20s %-20s\n", "Character Name", "Total Health", "Total Mana", "Spell");
-        System.out.printf("%-20s %-,20d %-,20d %-20s\n", myCharacter.getName(), myCharacter.getHealth(), 
+        System.out.printf("%-20s %-,20d %-,20d %-20s\n", myCharacter.getName(), myCharacter.getHealth(),
                 myCharacter.getMana(), myCharacter.getSpell());
-        
+
         keyboard.close();
-        
-    } 
+
+    }
 }
